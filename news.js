be363ab92a7ce047c05b1084d8bf0fd57e34a98f
miguelneto0 <miguelneto0019@gmail.com>
2021-01-07 13:09:36 -0300

    adicao imagens programas e pagina nova

---
+++ news.js
@@ -0,0 +1,6 @@
+function changeNews(){
+    var a = document.getElementsByClassName("li-news").src;
+    var b = document.getElementsByClassName("li-news").img.src; 
+
+    console.log(a+" "+b)
+}   

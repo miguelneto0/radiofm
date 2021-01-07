7b12df55e4754e50f3655b19a62501e4282031a2
miguelneto0 <miguelneto0019@gmail.com>
2021-01-07 01:19:58 -0300

    adicao da radio e de novos estilos

--- pageone.js
+++ pageone.js
@@ -1,5 +1,7 @@
 window.onscroll = function() {scrollFunction()};
+window.onload = trocaProg();
 
+// funcao de reduzir a logo do menu
 console.log(document.documentElement.scrollTop);
 
 function scrollFunction() {
@@ -13,4 +15,51 @@ function scrollFunction() {
     document.getElementById("image-logo").style.width = "20%";
     document.getElementById("player").style.background = "black";
   }
+}
+
+// funcao pra atualizar o programa q esta tocando
+function trocaProg(){
+  var hora = new Date();
+
+  if(hora.getHours() >= 6 && hora.getHours() < 7){
+    document.getElementById("toca").innerHTML = "Igreja Shalom";
+    document.getElementById("hora").innerHTML = "06:00 às 07:00";
+  }else if(hora.getHours() >= 7 && hora.getHours() < 10){
+    document.getElementById("toca").innerHTML = "Tribuna 101";
+    document.getElementById("hora").innerHTML = "07:00 às 10:00";
+  }else if(hora.getHours() >= 10 && hora.getHours() < 13){
+    document.getElementById("toca").innerHTML = "Estação Cidade";
+    document.getElementById("hora").innerHTML = "10:00 às 13:00";
+  }else if(hora.getHours() >= 13 && hora.getHours() < 14){
+    document.getElementById("toca").innerHTML = "Celebrai";
+    document.getElementById("hora").innerHTML = "13:00 às 14:00";
+  }else if(hora.getHours() >= 14 && hora.getHours() < 16){
+    document.getElementById("toca").innerHTML = "Fuzooê";
+    document.getElementById("hora").innerHTML = "14:00 às 16:00";
+  }else if(hora.getHours() >= 16 && hora.getHours() < 18){
+    document.getElementById("toca").innerHTML = "Puxa o fole";
+    document.getElementById("hora").innerHTML = "16:00 às 18:00";
+  }else if(hora.getHours() >= 18 && hora.getHours() < 19){
+    document.getElementById("toca").innerHTML = "Por do Sol";
+    document.getElementById("hora").innerHTML = "18:00 às 19:00";
+  }else if(hora.getHours() >= 19 && hora.getHours() < 20 ){
+    document.getElementById("toca").innerHTML = "Voz do Brasil";
+    document.getElementById("hora").innerHTML = "19:00 às 20:00";
+  }else if(hora.getHours() >= 20 && hora.getHours() <= 23){
+    document.getElementById("toca").innerHTML = "Voo Noturno";
+    document.getElementById("hora").innerHTML = "20:00 às 23:59";
+  }else if(hora.getHours() >= 0 && hora.getHours() < 7){
+    document.getElementById("toca").innerHTML = "De Volta Pro Futuro";
+    document.getElementById("hora").innerHTML = "00:00 às 06:00";
+  }
+  var dia = "";
+  var mes = "";
+
+  if(hora.getDay() == 4)
+    dia = "QUI";
+  if(hora.getMonth() == 0)
+    mes = "Janeiro";
+  document.getElementById("dia").innerHTML = " <b>" + dia + "</b> &nbsp;" + hora.getHours() + ":" + hora.getMinutes() + " - &nbsp;" + hora.getDate() + " de " + mes + " de " + hora.getFullYear();
+
+  console.log(hora.getDate() + " - " + hora.getDay() + " - " + hora.getFullYear())
 }

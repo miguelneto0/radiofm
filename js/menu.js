15f77115375cc499099671ee719362417210a3da
miguelneto0 <miguelneto0019@gmail.com>
2021-01-31 19:32:08 -0300

    menu responsivo

--- js/menu.js
+++ js/menu.js
@@ -2,28 +2,6 @@ function toggleMenu(){
     console.log('menu clicado')
     var lis = document.getElementsByClassName('li-normal')
     console.log(lis)
-    // if (lis[0].style.display = "none"){
-    //     console.log('esta escondido')
-    //     for(let i=0;i<lis.length;i++)
-    //         lis[i].style.display = "inline-block"
-    // }else if(lis[0].style.display = "inline-block"){
-    //     console.log('esta visivel')
-    //     for(let i=0;i<lis.length;i++)
-    //         lis[i].style.display = "none"
-    // }
-
-    switch (lis[0].style.display) {
-        case "none":
-            console.log('esta escondido')
-            for(let i=0;i<lis.length;i++)
-                lis[i].style.display = "inline-block"
-            break;
-        case "inline-block":
-            console.log('esta visivel')
-            for(let i=0;i<lis.length;i++)
-                lis[i].style.display = "none"
-            break;
-        default:
-            break;
-    }
+    for(let i=0;i<lis.length;i++)
+        lis[i].classList.toggle("showli")
 }
---
+++ js/programacao.js
@@ -0,0 +1,15 @@
+var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
+var days = document.getElementsByClassName('week')
+
+if(width <= 950)
+    for(let i=0;i<days.length;i++)
+        days[i].style.fontSize = "14pt"
+    days[0].innerHTML = "DOM"
+    days[1].innerHTML = "SEG"
+    days[2].innerHTML = "TER"
+    days[3].innerHTML = "QUA"
+    days[4].innerHTML = "QUI"
+    days[5].innerHTML = "SEX"
+    days[6].innerHTML = "SAB"
+

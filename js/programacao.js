f65952c12a23e0aadb9739ce40ecf726dea0c894
miguelneto0 <miguelneto0019@gmail.com>
2021-02-07 20:30:09 -0300

    Novos elementos reponsivos em programacao e Aovivo pageone

--- js/programacao.js
+++ js/programacao.js
@@ -1,11 +1,14 @@
+window.onload = prog();
+
+function prog(){
 var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 
 var days = document.getElementsByClassName('week')
+console.log(width)
 
 if(width <= 950){
-    console.log("mudar....")
     for(let i=0;i<days.length;i++){
-        days[i].style.fontSize = "14pt"
+        days[i].style.fontSize = "1.3vw"
         days[0].innerHTML = "DOM"
         days[1].innerHTML = "SEG"
         days[2].innerHTML = "TER"
@@ -14,4 +17,24 @@ if(width <= 950){
         days[5].innerHTML = "SEX"
         days[6].innerHTML = "SAB"
     }
+}else if(width <= 450){
+    for(let j=0;j<days.length;j++){
+        days[j].style.fontSize = "1.3vw"
+        days[0].innerHTML = "D"
+        days[1].innerHTML = "S"
+        days[2].innerHTML = "T"
+        days[3].innerHTML = "Q"
+        days[4].innerHTML = "Q"
+        days[5].innerHTML = "S"
+        days[6].innerHTML = "S"
+    }
+}
+var liweek = document.getElementsByClassName('week')
+for(let i=0;i<liweek.length;i++){
+    liweek[i].style.marginLeft = "2%"
+    liweek[i].style.fontSize = "2vh"
+}
+
+document.querySelector('ul.weekdays').style.width = "100%"
+
 }

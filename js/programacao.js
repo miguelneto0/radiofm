6a94ae83427f1675ce2c99f83ecf723b79fe9a37
miguelneto0 <miguelneto0019@gmail.com>
2021-02-07 00:48:13 -0300

    Adicionando elementos responsivos para Sec1 e Sec2

--- js/programacao.js
+++ js/programacao.js
@@ -2,14 +2,16 @@ var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 
 var days = document.getElementsByClassName('week')
 
-if(width <= 950)
-    for(let i=0;i<days.length;i++)
+if(width <= 950){
+    console.log("mudar....")
+    for(let i=0;i<days.length;i++){
         days[i].style.fontSize = "14pt"
-    days[0].innerHTML = "DOM"
-    days[1].innerHTML = "SEG"
-    days[2].innerHTML = "TER"
-    days[3].innerHTML = "QUA"
-    days[4].innerHTML = "QUI"
-    days[5].innerHTML = "SEX"
-    days[6].innerHTML = "SAB"
-
+        days[0].innerHTML = "DOM"
+        days[1].innerHTML = "SEG"
+        days[2].innerHTML = "TER"
+        days[3].innerHTML = "QUA"
+        days[4].innerHTML = "QUI"
+        days[5].innerHTML = "SEX"
+        days[6].innerHTML = "SAB"
+    }
+}
